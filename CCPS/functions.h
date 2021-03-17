83b6a8163ea380086463695a4984c0ee64d6dcf0
riliks <72131460+riliks@users.noreply.github.com>
2021-03-17 13:42:57 +0300

    Delete CCPS directory

--- CCPS/functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-#include <string>
-
-bool authorize(std::string login, std::string password);//авторизация
-
-bool registration(std::string login, std::string password);//регистрация
-
-#endif // FUNCTIONS_H
